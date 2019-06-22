ade4c7fe2d802d72c747387bb3a84c04c0498e01
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2019-06-22 17:21:43 -0400

    Add a test

--- catkin_ws/src/aquadrone_description/test/test_models.py
+++ catkin_ws/src/aquadrone_description/test/test_models.py
@@ -6,8 +6,12 @@ import rostest
 
 class TestV2Loaded(unittest.TestCase):
 
-    def test_param_loaded(self):
+    def test_base_param_loaded(self):
         val = rospy.get_param("/v2_description", None)
+        self.assertIsNotNone(val)
+
+    def test_wobbly_param_loaded(self):
+        val = rospy.get_param("/v2_wobbly_description", None)
         self.assertIsNotNone(val)
 
 if __name__ == '__main__':
