79921de32b209e0a5e3ba08adbc649ed08922532
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2019-07-08 19:44:33 -0400

    Add handling of geometry description

--- catkin_ws/src/thruster_control/src/thruster_control/configurations/v2_configuration.py
+++ catkin_ws/src/thruster_control/src/thruster_control/configurations/v2_configuration.py
@@ -1,4 +1,25 @@
 import numpy as np
+import math
+
+import geometry_helper as gh
+
+def get_thrusts_to_wrench():
+
+    in2m = 2.54 * 0.01
+
+    dX = 36.0 * in2m * 0.5
+    dY = 12.0 * in2m * 0.5
+
+    a2 = math.pi / 2.0
+    a4 = math.pi / 4.0
+
+    th_0 = gh.get_thruster_wrench_vector(x=dX,  y=dY, z=0, roll=0, pitch=a4, yaw=a2)
+    th_1 = gh.get_thruster_wrench_vector(x=dX,  y=-dY, z=0, roll=0, pitch=a4, yaw=-a2)
+    th_2 = gh.get_thruster_wrench_vector(x=0,   y=dY, z=0, roll=0, pitch=0, yaw=0)
+    th_3 = gh.get_thruster_wrench_vector(x=0,   y=-dY, z=0, roll=0, pitch=0, yaw=0)
+    th_4 = gh.get_thruster_wrench_vector(x=-dX, y=dY, z=0, roll=0, pitch=a4, yaw=a2)
+    th_5 = gh.get_thruster_wrench_vector(x=-dX, y=-dY, z=0, roll=0, pitch=a4, yaw=-a2)
+    return np.column_stack((th_0, th_1, th_2, th_3, th_4, th_5))
 
 def get_wrench_to_thrusts_lb_in():
 
