710cf6bfea82167bb5434a6caf339205526082bf
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2019-07-12 13:17:33 -0400

    Improve depth control with pid

--- catkin_ws/src/thruster_control/nodes/demo_depth_control.py
+++ catkin_ws/src/thruster_control/nodes/demo_depth_control.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import rospy
+from simple_pid import PID
 
 from std_msgs.msg import Float64
 from geometry_msgs.msg import Wrench
@@ -11,8 +12,16 @@ class DemoDepthController:
     def __init__(self):
         rospy.init_node('depth_control')
         self.depth = 0
-        self.goal = 5
-        self.kP = 100.0
+
+        self.Kp = 100
+        self.Kd = 20
+        self.Ki = 0.05
+
+        self.loop_rate = 10.0
+        self.rate = rospy.Rate(self.loop_rate)
+        self.pid = PID(self.Kp, self.Ki, self.Kd, setpoint=5)
+        self.pid.output_limits = (-50, 50)
+        self.pid.sample_time = 1.0 / self.loop_rate 
 
         # In future, take in state estimation, and not sensor reading
         self.depth_sub = rospy.Subscriber('aquadrone_v2/out/pressure', FluidPressure, self.depth_cb)
@@ -32,10 +41,12 @@ class DemoDepthController:
 
     def goal_cb(self, msg):
         self.goal = msg.data
+        self.pid.setpoint = self.goal
 
 
     def depth_cb(self, msg):
         self.depth = self.pressure_to_m(msg.fluid_pressure)
+        print(self.depth)
 
     def pressure_to_m(self, p):
         return (p - self.pressure_offset) / self.g
@@ -43,12 +54,10 @@ class DemoDepthController:
     def run(self):
 
         while not rospy.is_shutdown():
-            err = self.depth - self.goal
-            u = self.kP * err
-
+            u = -self.pid(self.depth)
             self.publish_wrench(u)
 
-            rospy.sleep(0.1)
+            self.rate.sleep()
 
     def publish_thrusts(self, u):
             self.send_command(self.th_0_pup, u)
