cbab5baee7f95e9941c2c28ceec6f21e417177ef
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2019-07-11 21:02:38 -0400

    Debug some building

--- catkin_ws/src/aquadrone_sensors/nodes/imu_node.py
+++ catkin_ws/src/aquadrone_sensors/nodes/imu_node.py
@@ -1,13 +1,16 @@
+#!/usr/bin/env python
+
 import rospy
 from aquadrone_msgs.msg import AquadroneIMU
-from sensor_msgs.msg import IMU
+from sensor_msgs.msg import Imu
 from Adafruit_BNO005 import BNO005
 
+#print(dir(BNO005))
 bno = BNO005.BNO055()
 
 rospy.init_node('aquadrone_IMU')
 pubUW = rospy.Publisher('/aquadrone_v2/out/SensorUW', aquadroneIMU)
-pubIMU = rospy.Publisher('/aquadrone_v2/out/imu', IMU)
+pubIMU = rospy.Publisher('/aquadrone_v2/out/imu', Imu)
 rate = rospy.Rate(10)
 
 while not rospy.is_shutdown():
@@ -17,7 +20,7 @@ while not rospy.is_shutdown():
   msgUW.accelerometer = bno.read_accelerometer()
   msgUW.linear_acceleration = bno.read_linear_acceleration()
   msgUW.quaternion = bno.read_quaternion()
-  msgIMU = IMU()
+  msgIMU = Imu()
   msgIMU.orientation = bno.read_quaternion()
   msgIMU.angular_velocity = bno.read_gyroscope()
   msgIMU.linear_acceleration = bno.read_linear_acceleration()
