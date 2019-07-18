9545aa8dc7ae56c8362a79c90b835d1703ab3d52
Leon Dai <linchuandai@gmail.com>
2019-07-18 00:01:16 -0400

    more global path finding code + points

--- global_path_finding.py
+++ global_path_finding.py
@@ -1,24 +1,39 @@
 from dataclasses import dataclass
+from points_helper import Cartesian_Point
 
-@dataclass
-class Point:
-    x: float = 0.0
-    y: float = 0.0
-    z: float = 0.0
-
-def get_next_move(points: [Point]):
+def get_next_point(points: [Cartesian_Point]) -> Cartesian_Point:
     # find  the middle x, y, z point
     x_dest = sum([point.x for point in points]) / len(points)
     y_dest = sum([point.y for point in points]) / len(points)
     z_dest = sum([point.z for point in points]) / len(points)
-
-    #
     
+    return Cartesian_Point(x_dest, y_dest, z_dest)
+
+def get_next_move():
     
-    return [x_dest, y_dest, z_dest]
+    # get coordinate of the destination by posting ros message to camera/target aquisition
+
+    # example of points to get back
+    point1 = Cartesian_Point(0.0, 0.0, 0.0)
+    point2 = Cartesian_Point(0.0, 4.0, 0.0)
+    point3 = Cartesian_Point(4.0, 0.0, 0.0)
+    point4 = Cartesian_Point(4.0, 4.0, 0.0)
+
+    next_point = get_next_point([point1, point2, point3, point4])
+
+    # determine next move to make, prioritize z and then x/y
+    if (next_point.z > 1):
+        next_move = Cartesian_Point(0.0, 0.0, 1.0)
+    else:
+        if (next_point.x > 1):
+            next_move = Cartesian_Point(1.0, 0.0, 0.0)
+        elif (next_point.y > 1):
+            next_move = Cartesian_Point(0.0, 1.0, 0.0)
+        else:
+            next_move = Cartesian_Point(0.0, 0.0, 0.0)
+
+    return next_move
+    # post next move to local path finding
 
 
-
-
-point1 = Point(0.0, 0.5, 0.1)
-print(get_next_move([point1, point1, point1, point1]))
+print(get_next_move())
