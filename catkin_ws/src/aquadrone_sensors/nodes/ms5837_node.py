7312af12f2463d4c8087d90331b47ecfeaa85d44
JoeOnTheGo33 <joeonthego100@gmail.com>
2019-10-07 18:19:22 -0400

    figured out how to leave node outside of sensor pkg

---
+++ catkin_ws/src/aquadrone_sensors/nodes/ms5837_node.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import rospy
+from sensor_msgs.msg import FluidPressure
+from ms5837_python import ms5837
+
+#-------
+port = 1
+#-------
+
+'''DepthSensor.msg
+float64 pressure
+float64 temperature
+float64 depth
+'''
+
+sensor = ms5837.MS5837_30BA(port)
+
+if not sensor.init():
+    rospy.core.logerr("unable to init ms5837, depth sensor")
+else:
+    pub = rospy.Publisher('/aquadrone_v2/out/depth', FluidPressure)
+    rospy.init_node('depth_sensor')
+    rate = rospy.Rate(10)
+
+    while not rospy.is_shutdown():
+        if sensor.read():
+            msg = FluidPressure()
+            msg.pressure = sensor.pressure()# mbar
+            pub.publish(msg)
+        else:
+            rospy.core.logwarn("unable to read ms5837, depth sensor")
+        rate.sleep()
+
+
