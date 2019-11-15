8c1e80b280309f2ff4b664520090ee4dad260c70
JoeOnTheGo33 <joeonthego100@gmail.com>
2019-11-14 20:42:21 -0500

    working depth sensor code, tested 2019-11-14

--- catkin_ws/src/aquadrone_sensors/nodes/ms5837_node.py
+++ catkin_ws/src/aquadrone_sensors/nodes/ms5837_node.py
@@ -8,22 +8,28 @@ from ms5837_python import ms5837
 port = 1
 #-------
 
-sensor = ms5837.MS5837_30BA(port)
+pub = rospy.Publisher('/aquadrone/out/pressure', FluidPressure, queue_size=1)
+rospy.init_node('depth_sensor')
+rate = rospy.Rate(10)
 
-if not sensor.init():
-    rospy.core.logerr("unable to init ms5837, depth sensor")
-else:
-    pub = rospy.Publisher('/aquadrone/out/pressure', FluidPressure)
-    rospy.init_node('depth_sensor')
-    rate = rospy.Rate(10)
+while not rospy.is_shutdown():
+    sensor = ms5837.MS5837_30BA(port)
 
-    while not rospy.is_shutdown():
-        if sensor.read():
-            msg = FluidPressure()
-            msg.pressure = sensor.pressure()# mbar
-            pub.publish(msg)
-        else:
-            rospy.core.logwarn("unable to read ms5837, depth sensor")
-        rate.sleep()
+    if not sensor.init():
+        rospy.core.logerr("unable to init ms5837, depth sensor")
+    else:
+        try:
+            while not rospy.is_shutdown():
+                if sensor.read():
+                    msg = FluidPressure()
+                    msg.fluid_pressure = sensor.pressure()# mbar
+                    pub.publish(msg)
+                else:
+                    rospy.core.logwarn("unable to read ms5837, depth sensor")
+                rate.sleep()
+        except IOError as ioerror:
+            print(ioerror)
+
+    rate.sleep()
 
 
