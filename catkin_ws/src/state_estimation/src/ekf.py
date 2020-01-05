8454a77af532ebb0ffd116851cdf4aa9b5d739fd
Wesley Fisher <wesley_b_fisher@hotmail.com>
2020-01-05 16:26:06 -0500

    Update dynamics model

--- catkin_ws/src/state_estimation/src/ekf.py
+++ catkin_ws/src/state_estimation/src/ekf.py
@@ -10,6 +10,33 @@ from autograd import grad, jacobian, elementwise_grad
 from geometry_msgs.msg import Point, Vector3, Quaternion
 from sensor_msgs.msg import Imu, FluidPressure
 
+from thruster_control.configurations.v28_configuration import V28Configuration
+from aquadrone_msgs.msg import SubState, MotorControls
+
+def quaternion_to_euler(orientation):
+        # From wikipedia (https://en.wikipedia.org/wiki/Conversion_between_quaternions_and_Euler_angles)
+
+        angles = Vector3()
+
+        # roll (x-axis rotation)
+        sinr_cosp = +2.0 * (orientation.w * orientation.x + orientation.y * orientation.z)
+        cosr_cosp = +1.0 - 2.0 * (orientation.x * orientation.x + orientation.y * orientation.y)
+        angles.x = math.atan2(sinr_cosp, cosr_cosp)
+
+        # pitch (y-axis rotation)
+        sinp = +2.0 * (orientation.w * orientation.y - orientation.z * orientation.x)
+        if np.abs(sinp) >= 1:
+            angles.y = np.sign(sinp) * math.pi # use 90 degrees if out of range
+        else:
+            angles.y = math.asin(sinp);
+
+        # yaw (z-axis rotation)
+        siny_cosp = +2.0 * (orientation.w * orientation.z + orientation.x * orientation.y);
+        cosy_cosp = +1.0 - 2.0 * (orientation.y * orientation.y + orientation.z * orientation.z);  
+        angles.z = math.atan2(siny_cosp, cosy_cosp);
+
+        return angles;
+
 class IDx:
     # Position
     Px = 0
@@ -44,6 +71,7 @@ class PressureSensorListener:
         self.g = 9.8
 
         self.calc_H = jacobian(self.state_to_measurement)
+
 
     def get_timeout_sec(self):
         return 0.1
@@ -87,14 +115,18 @@ class PressureSensorListener:
 
 
 class EKF:
-    def __init__(self):
+    def __init__(self, config):
         # https://en.wikipedia.org/wiki/Extended_Kalman_filter
 
         self.n = 13
         self.m = 8
 
         self.x = np.zeros((self.n, 1))
+        self.x[IDx.Ow] = 1
+
         self.u = np.zeros((self.m,1))
+
+        self.B = np.array(config.get_thrusts_to_wrench_matrix())
 
         self.P = np.eye(self.n)
         self.Q = np.eye(self.n) * 0.01
@@ -106,6 +138,13 @@ class EKF:
 
         self.calc_F = jacobian(self.f)
 
+        self.sub_state_msg = SubState()
+        self.state_pub = rospy.Publisher("state_estimation", SubState, queue_size=1)
+
+        self.motor_sub = rospy.Subscriber("motor_command", MotorControls, self.motor_cb)
+
+    def motor_cb(self, msg):
+        self.u = np.array(msg.motorThrusts)
 
     def prediction(self):
         F = self.calc_F(self.x, self.u)
@@ -171,26 +210,143 @@ class EKF:
 
     def f(self, x, u):
         n = x.shape[0]
+        dt = 1.0 / self.rate
 
-        x_out = x
+        motor_wrench = np.dot(self.B, self.u)
+
+        new_pos = np.array([x[IDx.Px] + dt*x[IDx.Vx],
+                            x[IDx.Py] + dt*x[IDx.Vy],
+                            x[IDx.Pz] + dt*x[IDx.Vz]])
+
+        mass = 10.0
+        accel = (self.calculate_linear_forces(x, motor_wrench)) * 1.0 / mass
+
+        new_vel = np.array([x[IDx.Vx],
+                            x[IDx.Vy],
+                            x[IDx.Vz] ]) + dt*accel
+
+
+        new_or = self.update_orientation_quaternion(x)
+
+        # Update with inertia
+        ang_acc = self.calculate_angular_forces(x, motor_wrench) * 1.0/mass
+        new_ang_vel = np.array([x[IDx.Ax],
+                                x[IDx.Ay],
+                                x[IDx.Az]])
+
+        x_out = np.vstack([new_pos, new_vel, new_or, new_ang_vel])
+
 
         #return np.dot(1.0*np.eye(n), x) + np.dot(2.0*np.eye(m), u)
         return x_out
 
-    def output(self):
-        print("X")
-        print(self.x)
-        print("P")
+    def calculate_linear_forces(self, x, wrench):
+        mass = 10.0
+        motor_forces = np.array(wrench[0:3])
+        motor_forces.shape = (3,1)
 
-        print(" ")
-        print(self.P)
+        drag_forces_linear = np.array([ -0.01 * x[IDx.Vx],
+                                        -0.01 * x[IDx.Vy],
+                                        -0.01 * x[IDx.Vz]])
+        try:
+            drag_forces_linear.shape = (3,1)
+        except Exception as e:
+            pass
+
+        drag_forces_quad = np.array([ -0.01 * x[IDx.Vx]*np.abs(x[IDx.Vx]),
+                                      -0.01 * x[IDx.Vy]*np.abs(x[IDx.Vy]),
+                                      -0.01 * x[IDx.Vz]*np.abs(x[IDx.Vz])])
+        try:
+            drag_forces_quad.shape = (3,1)
+        except Exception as e:
+            pass
+
+        return motor_forces + drag_forces_linear + drag_forces_quad
+
+    def calculate_angular_forces(self, x, wrench):
+        motor_forces = np.array(wrench[0:3])
+        motor_forces.shape = (3,1)
+
+        # TODO: Add drag
+
+        return motor_forces
+        
+
+    def update_orientation_quaternion(self, x):
+        dt = 1.0 / self.rate
+        # https://gamedev.stackexchange.com/questions/108920/applying-angular-velocity-to-quaternion
+        q_old = np.array([x[IDx.Ow],
+                          x[IDx.Ox],
+                          x[IDx.Oy],
+                          x[IDx.Oz]])
+        try:
+            q_old.shape = (4,1)
+        except Exception as e:
+            pass
+        
+        w = np.array([0,
+                      x[IDx.Ax],
+                      x[IDx.Ay],
+                      x[IDx.Az]])[np.newaxis]
+        w = np.transpose(w)
+        try:
+            w.shape = (4,1)
+        except Exception as e:
+            pass
+
+        new_or = q_old + 0.5 * dt * w
+        
+        try:
+            new_or.shape = (4,1)
+        except Exception as e:
+            pass
+        mag = np.linalg.norm(new_or)
+        new_or = new_or / mag
+        return new_or
+
+    def output(self):
+        var = np.array(np.diagonal(self.P))
+        var.shape = (self.n, 1)
+        
+        print("X, Var:")
+        print(np.hstack([self.x, var]))
+
+    def fill_vector_value_variance(self, val, var, x, P, i0):
+        val.x = x[i0 + 0]
+        val.y = x[i0 + 1]
+        val.z = x[i0 + 2]
+
+        var.x = P[i0+0, i0+0]
+        var.y = P[i0+1, i0+1]
+        var.z = P[i0+2, i0+2]
+
+
+    def update_state_msg(self):
+        self.fill_vector_value_variance(self.sub_state_msg.position,
+                                        self.sub_state_msg.pos_variance,
+                                        self.x, self.P, IDx.Px)
+        self.fill_vector_value_variance(self.sub_state_msg.velocity,
+                                        self.sub_state_msg.vel_variance,
+                                        self.x, self.P, IDx.Vx)
+
+        quat = Quaternion()
+        quat.x = self.x[IDx.Ox]
+        quat.y = self.x[IDx.Oy]
+        quat.z = self.x[IDx.Oz]
+        quat.w = self.x[IDx.Ow]
+        self.sub_state_msg.orientation_quat = quat
+        self.sub_state_msg.orientation_RPY = quaternion_to_euler(quat)
+
 
     def run(self):
         while not rospy.is_shutdown():
             self.rate_ctrl.sleep()
-            self.output()
-            self.prediction()
 
+            self.output()
+            self.update_state_msg()
+            self.state_pub.publish(self.sub_state_msg)
+
+            self.prediction()
             self.update()
             
             
@@ -198,5 +354,8 @@ class EKF:
 if __name__ == "__main__":
     rospy.init_node("simple_state_estimation")
 
-    ekf = EKF()
+    config = V28Configuration()
+    config.initialize()
+
+    ekf = EKF(config)
     ekf.run()
