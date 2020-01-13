94513731e301dc6cba7ac0021784fd6a5e203ee5
Amaar Quadri <amaarquadri@gmail.com>
2020-01-13 15:18:33 -0500

    Fix topic names to match ekf, increase update rate, fix typos

--- catkin_ws/src/aquadrone_sensors/nodes/imu_node.py
+++ catkin_ws/src/aquadrone_sensors/nodes/imu_node.py
@@ -9,22 +9,24 @@ from Adafruit_BNO005 import BNO005
 bno = BNO005.BNO055()
 
 rospy.init_node('aquadrone_IMU')
-pubUW = rospy.Publisher('/aquadrone_v2/out/SensorUW', aquadroneIMU)
-pubIMU = rospy.Publisher('/aquadrone_v2/out/imu', Imu)
-rate = rospy.Rate(10)
+pubUW = rospy.Publisher('/aquadrone/out/SensorUW', AquadroneIMU)
+pubIMU = rospy.Publisher('/aquadrone/out/imu', Imu)
+rate = rospy.Rate(50)  # The BNO005 outputs all IMU data at 100Hz
+
 
 while not rospy.is_shutdown():
-  msgUW = aquadroneIMU()
+  msgUW = AquadroneIMU()
   msgUW.euler = bno.read_euler()
   msgUW.gyroscope = bno.read_gyroscope()
   msgUW.accelerometer = bno.read_accelerometer()
   msgUW.linear_acceleration = bno.read_linear_acceleration()
   msgUW.quaternion = bno.read_quaternion()
+
   msgIMU = Imu()
   msgIMU.orientation = bno.read_quaternion()
   msgIMU.angular_velocity = bno.read_gyroscope()
   msgIMU.linear_acceleration = bno.read_linear_acceleration()
+
   pubUW.publish(msgUW)
   pubIMU.publish(msgIMU)
   rate.sleep()
-Col
