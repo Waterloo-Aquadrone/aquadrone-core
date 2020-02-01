b4eadcac1f023a81af0625021e98aaf2d40f8f2b
Wesley Fisher <wesley_b_fisher@hotmail.com>
2020-02-01 10:52:19 -0500

    Remove old thruster nodes

--- catkin_ws/src/thruster_control/src/thruster_control/thruster_control_manager.py
+++ catkin_ws/src/thruster_control/src/thruster_control/thruster_control_manager.py
@@ -4,6 +4,7 @@ import rospy
 
 from aquadrone_msgs.msg import MotorControls 
 
+# TODO see if this can be used for both real and fake cases
 class SimThrusterController:
     def __init__(self, config, mcc, interface, thruster_specs):
         self.config = config
@@ -13,6 +14,9 @@ class SimThrusterController:
 
         # Will need motor commands published for state estimation
         self.publisher = rospy.Publisher("motor_command", MotorControls, queue_size=0)
+        
+        # Listen to what we publish, so that we can also turn controls off
+        self.subscriber = rospy.Subscriber("motor_command", MotorControls, callback=self.process_thrust_command)
 
 
     def run(self):
@@ -23,9 +27,11 @@ class SimThrusterController:
     def control_loop(self):
         w = self.mcc.get_recent_thrusts()
         thrusts = self.wrench_to_thrust_list(w)
-
         self.publish_command(thrusts)
 
+
+    def process_thrust_command(self, msg):
+        thrusts = msg.motorThrusts
         for i in range(0, self.config.get_num_thrusters()):
             thrust = thrusts[i]
             sig = self.thruster_specs[i].thrust_to_signal(thrust)
