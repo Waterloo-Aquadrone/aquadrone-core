11477306f5c3dc9fe244b408e7cff9564e442279
MubshAli <mubshali@gmail.com>
2020-02-15 14:22:51 -0500

    Create go_through_gate.py

---
+++ path_planning/src/path_planning/go_through_gate.py
@@ -0,0 +1,49 @@
+import math
+from data_structures import Gate
+from pole_finder_demo_states import BaseState
+from ros_modules import ROSControlsModule
+class GoThroughGate(BaseState):
+    def __init__(self,do_roll,go_left,go_right):
+        self.do_roll = do_roll
+        self.go_left = go_left
+        self.go_right = go_right
+
+    def state_name(self):
+        return "init_state"
+
+    def initialize(self, t, controls, sub_state, world_state, sensors):
+        pass
+
+    def finalize(self, t, controls, sub_state, world_state, sensors):
+        pass
+
+    def process(self, t, controls, sub_state, world_state, sensors):
+        """
+        sub_state
+        """
+        sub_pos = sub_state.get_submarine_state().position
+        gate_pos = world_state.get_gate_state().position
+        controls.set_depth_goal(gate_pos.z)
+        ##for yaw
+        x_coord = gate_pos.x - sub_pos.x
+        y_coord = gate_pos.y - sub_pos.y
+        x = (y_coord)/(x_coord)
+        angle =  math.atan(x)
+        controls.set_yaw_goal(angle)
+        ##for yaw
+        ##if(self.do_roll ):
+        while(sub_pos.x <= gate_pos.x):
+            planar_move_command(1,0,0)
+        if(self.go_right):
+            
+            planar_move_command(1,0,0)
+        elif(self.go_left):
+            planar_move_command(1,0,0)
+
+
+
+        
+
+        pass
+
+
