550a39e981c1041ddda6366dee91cc0cd4b8cb31
MubshAli <mubshali@gmail.com>
2020-02-15 17:52:45 -0500

    Update go_through_gate.py
    All's good

--- path_planning/src/path_planning/go_through_gate.py
+++ path_planning/src/path_planning/go_through_gate.py
@@ -7,6 +7,8 @@ class GoThroughGate(BaseState):
         self.do_roll = do_roll
         self.go_left = go_left
         self.go_right = go_right
+        self.gotten_to_gate = False
+        self.got_through_gate = False
 
     def state_name(self):
         return "init_state"
@@ -21,26 +23,47 @@ class GoThroughGate(BaseState):
         """
         sub_state
         """
+        
         sub_pos = sub_state.get_submarine_state().position
         gate_pos = world_state.get_gate_state().position
         controls.set_depth_goal(gate_pos.z)
-        ##for yaw
-        x_coord = gate_pos.x - sub_pos.x
-        y_coord = gate_pos.y - sub_pos.y
-        x = (y_coord)/(x_coord)
-        angle =  math.atan(x)
-        controls.set_yaw_goal(angle)
-        ##if(self.do_roll ):
-        while(sub_pos.x <= gate_pos.x):
-            planar_move_command(1,0,0)
+        x_c = gate_pos.x - sub_pos.x
+        y_c = gate_pos.y - sub_pos.y
+        distance_between_gate = math.sqrt((x_c*x_c)+(y_c*y_c))
+                ##for yaw
+        if(distance_between_gate > 0.1 and self.gotten_to_gate == False):
+            x = (x_c)/(y_c)
+            angle =  math.atan(x)
+            planar_move_command(0,0,0)
+            controls.set_yaw_goal(angle)
+            if(abs(sub_state.get_submarine_state().orientation_rpy.z - angle) < 0.1):
+                planar_move_command(1,0,0)
+            return
+        else:
+            self.gotten_to_gate = True
         ##when the sub reaches the gate, it pitches
-        planar_move_command(0,1,0)
-        planar_move_command(0,-2,0)
-        ##pitch done 
-        planar_move_command(0,1,0)
-        if(self.go_right):
+        # planar_move_command(0,1,0)
+        # planar_move_command(0,-2,0)
+        #pitch done 
+        # planar_move_command(0,1,0)
+                
+        ##straighten the submarine
+                
+        
+        # if(self.go_right):
+        #     planar_move_command(1,0,0)
+        # elif(self.go_left):
+        #     planar_move_command(1,0,0)
+                
+        length = sublength
+        if(distance_between_gate < length and self.gotten_to_gate == True):
             planar_move_command(1,0,0)
-        elif(self.go_left):
-            planar_move_command(1,0,0)
+        else:
+            self.got_through_gate = True
+        return
+        
+    def gone_through_gate():
+        return self.got_through_gate
+
 
 
