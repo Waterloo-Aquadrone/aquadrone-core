b0a53ba02c20cb2df1618c4747d513e2be6f9f4a
Wesley Fisher <wfisher@edu.uwaterloo.ca>
2020-02-22 18:57:37 -0500

    start tuning PID for rex_rov parameters. Add parameter server usage

--- thruster_control/nodes/demo_depth_control.py
+++ thruster_control/nodes/demo_depth_control.py
@@ -6,5 +6,11 @@ from thruster_control.depth_pid_controller import DepthPIDController
 
 
 if __name__ == "__main__":
-    ddc = DepthPIDController()
+    rospy.init_node('depth_control')
+
+    Kp = rospy.get_param('/stability/depth/Kp')
+    Kd = rospy.get_param('/stability/depth/Kd')
+    Ki = rospy.get_param('/stability/depth/Ki')
+
+    ddc = DepthPIDController(Kp=Kp, Ki=Ki, Kd=Kd)
     ddc.run()
