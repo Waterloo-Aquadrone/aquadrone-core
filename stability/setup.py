a9611ac413913b66e9ed29914cd6e49e4b15fde3
Amaar Quadri <amaarquadri@gmail.com>
2020-05-29 01:26:38 -0400

    added top level python packages with name matching ros package, updated setup.py

--- stability/setup.py
+++ stability/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['depth_control']
+d['packages'] = ['stability']
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
