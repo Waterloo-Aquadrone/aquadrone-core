bb1cc9826f6ec1bd4c4c518b44e4cd942a1f0614
Amaar Quadri <amaarquadri@gmail.com>
2020-05-29 01:16:09 -0400

    updated setup.py with new package names, created setup.py in stability package removed redundant init.py files

--- stability/setup.py
+++ stability/setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['thrust_computer', 'thrust_distributor', 'real_thruster_types']
+d['packages'] = ['depth_control']
 d['package_dir'] = {'': 'src'}
 
 setup(**d)
