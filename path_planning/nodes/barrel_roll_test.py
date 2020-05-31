e2a0942943d0fc5a1ffeecf2839a8998462b9f06
Amaar Quadri <amaarquadri@gmail.com>
2020-05-30 23:40:07 -0400

    added parallel execution state machine

--- path_planning/nodes/barrel_roll_test.py
+++ path_planning/nodes/barrel_roll_test.py
@@ -7,6 +7,7 @@ from path_planning.states.stabilize_state import StabilizeState
 from path_planning.states.go_to_depth import GoToDepthState
 from path_planning.states.barrel_roll import BarrelRoll
 from path_planning.states.exit_code_state import ExitCodeState
+from path_planning.state_machines.parallel_state_machine import ParallelStateMachine
 from path_planning.state_machines.timed_state_machine import TimedStateMachine
 from path_planning.state_machines.sequential_state_machine import SequentialStateMachine
 from path_planning.state_machines.markov_chain_state_machine import MarkovChainStateMachine
@@ -18,15 +19,17 @@ if __name__ == "__main__":
 
     timeout = 60  # seconds
 
-    starting_states = [WaitingState(10), StabilizeState(), GoToDepthState(6)]
+    stabilise_at_depth_machine = ParallelStateMachine('stabilise_at_depth', [StabilizeState(), GoToDepthState(6)])
+    dive_machine = SequentialStateMachine('dive', [WaitingState(10), stabilise_at_depth_machine])
     timed_barrel_roll_state = TimedStateMachine(BarrelRoll(), timeout, timeout_exit_code=1)
-    success_states = [GoToDepthState(0), WaitingState(10), ExitCodeState(0)]
-    failure_states = [StabilizeState(), GoToDepthState(0), WaitingState(10), ExitCodeState(1)]
+    success_surface_machine = SequentialStateMachine('surface', [GoToDepthState(0), WaitingState(10), ExitCodeState(0)])
+    failure_surface_machine = SequentialStateMachine('stabilize_and_surface', [StabilizeState(), GoToDepthState(0),
+                                                                               WaitingState(10), ExitCodeState(1)])
 
-    states, dictionaries = zip((SequentialStateMachine(starting_states), {0: 1}),        # 0
-                               (timed_barrel_roll_state,                 {0: 2, 1: 3}),  # 1
-                               (SequentialStateMachine(success_states),  {0: -1}),       # 2
-                               (SequentialStateMachine(failure_states),  {0: -1}))       # 3
+    states, dictionaries = zip((dive_machine,            {0: 1}),        # 0
+                               (timed_barrel_roll_state, {0: 2, 1: 3}),  # 1
+                               (success_surface_machine, {0: -1}),       # 2
+                               (failure_surface_machine, {0: -1}))       # 3
     machine = MarkovChainStateMachine('barrel_roll_test', states, dictionaries)
     success = run_state(machine, rospy.Rate(5))
 
---
+++ path_planning/src/path_planning/state_machines/parallel_state_machine.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+from path_planning.states.base_state import BaseState
+
+
+class ParallelStateMachine(BaseState):
+    """
+    This state machine will simultaneous run all of the states assigned to it.
+    Note that the states must take care not to interfere with each other.
+    Ideally, there should be at most one state that interacts with each of
+    the depth control, orientation stability, and movement command systems.
+
+    The exit code of this state machine is the exit code of the state machine that is last in the list provided.
+    """
+
+    def __init__(self, name, states):
+        self.name = name
+        self.states = states
+        self.idx = 0
+        self.completed = False
+
+    def state_name(self):
+        return self.name + \
+               '/[' + ', '.join(state.state_name() for state in self.states if not state.has_completed()) + \
+               ']'
+
+    def initialize(self, t, controls, sub_state, world_state, sensors):
+        for state in self.states:
+            state.initialize(t, controls, sub_state, world_state, sensors)
+
+    def process(self, t, controls, sub_state, world_state, sensors):
+        for state in self.states:
+            state.process(t, controls, sub_state, world_state, sensors)
+
+    def finalize(self, t, controls, sub_state, world_state, sensors):
+        for state in self.states:
+            state.finalize(t, controls, sub_state, world_state, sensors)
+
+    def has_completed(self):
+        for state in self.states:
+            if not state.has_completed():
+                return False
+        return True
+
+    def exit_code(self):
+        # return the exit code of the last state
+        return self.states[-1].exit_code()
