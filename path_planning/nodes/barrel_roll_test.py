f79cc84dbb2f85420bef4f8be99dd26aa4760b50
Amaar Quadri <aquadri@uwaterloo.ca>
2020-09-07 19:18:37 -0500

    runtime error fixes

--- path_planning/nodes/barrel_roll_test.py
+++ path_planning/nodes/barrel_roll_test.py
@@ -34,7 +34,7 @@ if __name__ == "__main__":
     executor = StateExecutor(machine, rospy.Rate(5))
     executor.run()
 
-    if executor.exit_code() == 0:
+    if executor.get_exit_code() == 0:
         print('Successfully completed barrel roll!')
     else:
         print('Aborted barrel roll attempt!')
