4f8423e8f4558803940754c0cd496098138ef358
Amaar Quadri <amaarquadri@gmail.com>
2020-09-09 05:50:39 -0400

    comment

--- state_estimation/src/state_estimation/ekf_indices.py
+++ state_estimation/src/state_estimation/ekf_indices.py
@@ -4,4 +4,5 @@ import numpy as np
 
 class Idx:
     NUM = 13
+    # 3 position, 4 orientation (quaternion), 3 velocity, 3 angular velocity
     x, y, z, Ow, Ox, Oy, Oz, Vx, Vy, Vz, Wx, Wy, Wz = np.arange(NUM)
