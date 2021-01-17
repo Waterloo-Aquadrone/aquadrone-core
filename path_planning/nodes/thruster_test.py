75ecd3dcf560abe7254e18f14f23b611d05262c9
Aquadrone Jetson <aquadrone@jetson.com>
2021-01-17 02:51:15 -0500

    increased wait time

--- path_planning/nodes/thruster_test.py
+++ path_planning/nodes/thruster_test.py
@@ -12,7 +12,7 @@ from path_planning.state_executor import StateExecutor
 if __name__ == "__main__":
     rospy.init_node("thruster_test")
 
-    machine = SequentialStateMachine('thruster_test', [WaitingState(10), ThrusterTestState(thruster_count=8, thrust_amplitude=3, thrust_period=5), WaitingState(10)])
+    machine = SequentialStateMachine('thruster_test', [WaitingState(20), ThrusterTestState(thruster_count=8, thrust_amplitude=3, thrust_period=5), WaitingState(10)])
 
     executor = StateExecutor(machine, rate=rospy.Rate(5))
     executor.run()
