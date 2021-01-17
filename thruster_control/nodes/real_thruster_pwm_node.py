cbb0988ce47b6e88973336ef7332f0e631c2769c
Aquadrone Jetson <aquadrone@jetson.com>
2021-01-17 01:50:54 -0500

    added print

--- thruster_control/nodes/real_thruster_pwm_node.py
+++ thruster_control/nodes/real_thruster_pwm_node.py
@@ -48,4 +48,5 @@ if __name__ == "__main__":
         ThrusterClass = config.get_thruster_class(i)
         thrusters.append(ThrusterClass(pwm_frequency, i, pca.channels[i2c_data[i]['i2c_index']]))
 
+    print('Thruster initialization complete')
     rospy.spin()
