279c5201abff81f52cc67e45a2b98059037add2d
Amaar Quadri <amaarquadri@gmail.com>
2021-01-22 01:24:32 -0500

    added yaw test state

--- path_planning/src/path_planning/states/stabilize_state.py
+++ path_planning/src/path_planning/states/stabilize_state.py
@@ -8,16 +8,15 @@ class StabilizeState(BaseState):
     system during initialization (which will remain active after finalization as well).
     """
 
-    def __init__(self, r=0, p=0, y=0, tolerance_degrees=5):
-        self.r = r
-        self.p = p
-        self.y = y
+    def __init__(self, roll=0, pitch=0, yaw=0, tolerance_degrees=5, velocity_tolerance_degrees=2):
+        self.r = roll
+        self.p = pitch
+        self.y = yaw
         self.tolerance = np.radians(tolerance_degrees)
-        self.velocity_tolerance = np.radians(2)  # 2 deg/s
+        self.velocity_tolerance = np.radians(velocity_tolerance_degrees)
         self.completed = False
 
-    @staticmethod
-    def state_name():
+    def state_name(self):
         return "stabilize_state"
 
     def initialize(self, t, controls, sub_state, world_state, sensors):
