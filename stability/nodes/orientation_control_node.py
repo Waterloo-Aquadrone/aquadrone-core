7950de681efda054f38a37c04bcd671927e14a2e
Amaar Quadri <amaarquadri@gmail.com>
2021-01-30 00:36:25 -0500

    Recreated orientation control node in Python using simple_PID

---
+++ stability/nodes/orientation_control_node.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+import rospy
+
+from stability.angular_pid_controller import AngularPIDController
+
+
+if __name__ == "__main__":
+    rospy.init_node('orientation_pid')
+
+    orientation_pid = AngularPIDController()
+    orientation_pid.run()
