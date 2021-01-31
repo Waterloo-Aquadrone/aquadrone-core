f6d96f37e64a08b971ba1b3bad7e9b78399d4ddf
Amaar Quadri <amaarquadri@gmail.com>
2021-01-31 18:08:18 -0500

    update to python stability controller, add movement controller

--- path_planning/src/path_planning/states/travel_state.py
+++ path_planning/src/path_planning/states/travel_state.py
@@ -50,6 +50,9 @@ class TravelState(BaseState):
                   str(self.target_z) + ', yaw=' + str(self.target_yaw) + ')')
 
     def process(self, t, controls, sub_state, world_state, sensors):
+        controls.set_movement_target(self.target_x, self.target_y)
+        controls.set_depth_goal(self.target_z)
+
         sub = sub_state.get_submarine_state()
         displacement = np.array([self.target_x - sub.position.x,
                                  self.target_y - sub.position.y])
@@ -64,41 +67,41 @@ class TravelState(BaseState):
             self.completed = True
             return
 
-        controls.set_depth_goal(self.target_z)
-
-        angle_to_target = np.arctan2(displacement[1], displacement[0])
-        # yaw to either face target or target angle, depending on how close we are
-        if dist > TravelState.fine_control_threshold:
-            controls.set_yaw_goal(angle_to_target)
-            # if we are not yet facing the right direction, focus on that and leave x-y motion until later
-            if abs_angle_difference(sub.orientation_rpy.z, angle_to_target) > 5:
-                controls.planar_move_command(0, 0)
-                return
-        else:
-            controls.set_yaw_goal(self.target_yaw)
-
-        # Calculations for the applied force
-
-        v_target = sum(v * displacement) * displacement / dist ** 2  # vector velocity in the direction of target
-        v_perp = v - v_target  # vector velocity perpendicular to target
-
-        heading = sub.orientation_rpy.z - angle_to_target
-        heading_trig = np.array([np.sin(heading), np.cos(heading)])
-        # interpolate k_p and k_d values using elliptical fit between the pure x and pure y values based on heading
-        k_p = TravelState.k_p_prod / np.linalg.norm(TravelState.k_p * heading_trig)
-        k_d = TravelState.k_d_prod / np.linalg.norm(TravelState.k_d * heading_trig)
-        k_d_perp = TravelState.k_d_prod / np.linalg.norm(TravelState.k_d[::-1] * heading_trig)
-
-        # TODO: v_perp is undamped in this scheme and will (in theory) oscillate sinusoidally indefinitely
-        absolute_forces = k_p * displacement - k_d * v_target - k_d_perp * v_perp
-
-        sin = np.sin(sub.orientation_rpy.z)
-        cos = np.cos(sub.orientation_rpy.z)
-        abs_to_sub_transform = np.array([[cos, -sin],
-                                         [sin, cos]])
-        relative_forces = np.dot(abs_to_sub_transform, absolute_forces)
-
-        controls.planar_move_command(relative_forces[0], relative_forces[1])
+        # controls.set_depth_goal(self.target_z)
+        #
+        # angle_to_target = np.arctan2(displacement[1], displacement[0])
+        # # yaw to either face target or target angle, depending on how close we are
+        # if dist > TravelState.fine_control_threshold:
+        #     controls.set_yaw_goal(angle_to_target)
+        #     # if we are not yet facing the right direction, focus on that and leave x-y motion until later
+        #     if abs_angle_difference(sub.orientation_rpy.z, angle_to_target) > 5:
+        #         controls.planar_move_command(0, 0)
+        #         return
+        # else:
+        #     controls.set_yaw_goal(self.target_yaw)
+        #
+        # # Calculations for the applied force
+        #
+        # v_target = sum(v * displacement) * displacement / dist ** 2  # vector velocity in the direction of target
+        # v_perp = v - v_target  # vector velocity perpendicular to target
+        #
+        # heading = sub.orientation_rpy.z - angle_to_target
+        # heading_trig = np.array([np.sin(heading), np.cos(heading)])
+        # # interpolate k_p and k_d values using elliptical fit between the pure x and pure y values based on heading
+        # k_p = TravelState.k_p_prod / np.linalg.norm(TravelState.k_p * heading_trig)
+        # k_d = TravelState.k_d_prod / np.linalg.norm(TravelState.k_d * heading_trig)
+        # k_d_perp = TravelState.k_d_prod / np.linalg.norm(TravelState.k_d[::-1] * heading_trig)
+        #
+        # # TODO: v_perp is undamped in this scheme and will (in theory) oscillate sinusoidally indefinitely
+        # absolute_forces = k_p * displacement - k_d * v_target - k_d_perp * v_perp
+        #
+        # sin = np.sin(sub.orientation_rpy.z)
+        # cos = np.cos(sub.orientation_rpy.z)
+        # abs_to_sub_transform = np.array([[cos, -sin],
+        #                                  [sin, cos]])
+        # relative_forces = np.dot(abs_to_sub_transform, absolute_forces)
+        #
+        # controls.planar_move_command(relative_forces[0], relative_forces[1])
 
     def has_completed(self):
         return self.completed
---
+++ stability/src/stability/movement_pid_controller.py
@@ -0,0 +1,56 @@
+import rospy
+from simple_pid import PID
+import numpy as np
+
+from geometry_msgs.msg import Vector3, Wrench
+
+from aquadrone_msgs.msg import SubState
+
+from scipy.spatial.transform import Rotation
+
+
+class OrientationPIDController:
+    def __init__(self, rate=None):
+        if rate is None:
+            self.rate = rospy.Rate(10)
+
+        self.pids = []
+        for axis in 'xy':
+            Kp = rospy.get_param('/stability/' + axis + '/Kp')
+            Ki = rospy.get_param('/stability/' + axis + '/Ki')
+            Kd = rospy.get_param('/stability/' + axis + '/Kd')
+
+            pid = PID(Kp, Ki, Kd)
+            pid.output_limits = (-50, 50)
+            pid.setpoint = 0  # all target angles initialized to 0
+            self.pids.append(pid)
+
+        self.quaternion = np.array([0, 0, 0, 1])
+        self.position = np.array([0, 0, 0])
+
+        rospy.Subscriber("/movement_target", Vector3, callback=self.goal_cb)
+        rospy.Subscriber('/state_estimation', SubState, callback=self.state_cb)
+        self.pub = rospy.Publisher('/movement_command', Wrench, queue_size=1)
+
+    def goal_cb(self, msg):
+        for pid, target in zip(self.pids, [msg.x, msg.y]):
+            pid.setpoint = target
+
+    def state_cb(self, msg):
+        self.quaternion = np.array([msg.orientation_quat.x, msg.orientation_quat.y,
+                                    msg.orientation_quat.z, msg.orientation_quat.w])
+        self.position = np.array([msg.position.x, msg.position.y])
+
+    def run(self):
+        control = Wrench()
+        while not rospy.is_shutdown():
+            absolute_force = np.array([self.pids[0](self.position[0]), self.pids[1](self.position[1]), 0])
+            relative_force = np.dot(Rotation.from_quat(self.quaternion).inv().as_matrix(), absolute_force)
+            for var, value in zip('xyz', relative_force):
+                setattr(control.force, var, value)
+            self.pub.publish(control)
+
+            try:
+                self.rate.sleep()
+            except rospy.ROSInterruptException:
+                break
