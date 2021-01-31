fe11ffc9f3575668d2abb7d1315abfb17a979bd7
Amaar Quadri <amaarquadri@gmail.com>
2021-01-31 15:40:48 -0500

    transform torques to submarine reference frame

--- stability/src/stability/angular_pid_controller.py
+++ stability/src/stability/angular_pid_controller.py
@@ -7,6 +7,7 @@ from geometry_msgs.msg import Vector3, Wrench
 from aquadrone_msgs.msg import SubState
 
 from aquadrone_math_utils.angle_math import normalize_angle
+from scipy.spatial.transform import Rotation
 
 
 class AngularPIDController:
@@ -31,7 +32,8 @@ class AngularPIDController:
             pid.error_map = AngularPIDController.normalize_angular_error
             self.pids.append(pid)
 
-        self.orientation = [0, 0, 0]  # roll, pitch, yaw
+        self.quaternion = np.array([0, 0, 0, 1])
+        self.orientation = np.array([0, 0, 0])  # roll, pitch, yaw
         rospy.Subscriber("/orientation_target", Vector3, callback=self.goal_cb)
         rospy.Subscriber('/state_estimation', SubState, callback=self.state_cb)
         self.pub = rospy.Publisher('/stability_command', Wrench, queue_size=1)
@@ -41,14 +43,17 @@ class AngularPIDController:
             pid.setpoint = target
 
     def state_cb(self, msg):
-        self.orientation = [msg.orientation_RPY.x, msg.orientation_RPY.y, msg.orientation_RPY.z]
+        self.quaternion = np.array([msg.orientation_quat.x, msg.orientation_quat.y,
+                                    msg.orientation_quat.z, msg.orientation_quat.w])
+        self.orientation = np.array([msg.orientation_RPY.x, msg.orientation_RPY.y, msg.orientation_RPY.z])
 
     def run(self):
         control = Wrench()
         while not rospy.is_shutdown():
-            # TODO: coordinate transformation???
+            absolute_torque = np.array([pid(orientation) for pid, orientation in zip(self.pids, self.orientation)])
+            relative_torque = np.dot(Rotation.from_quat(self.quaternion).inv().as_matrix(), absolute_torque)
             for i, var in enumerate(['x', 'y', 'z']):
-                setattr(control.torque, var, self.pids[i](self.orientation[i]))
+                setattr(control.torque, var, relative_torque[i])
             self.pub.publish(control)
 
             try:
