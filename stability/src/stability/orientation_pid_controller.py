ceb23182e382b1a06749df655d1868ea50b640e2
Amaar Quadri <amaarquadri@gmail.com>
2021-02-03 00:33:11 -0500

    try switching to quaternion pid again

--- stability/src/stability/orientation_pid_controller.py
+++ stability/src/stability/orientation_pid_controller.py
@@ -1,13 +1,11 @@
 import rospy
-from simple_pid import PID
 import numpy as np
 
 from geometry_msgs.msg import Vector3, Wrench
-
 from aquadrone_msgs.msg import SubState
 
 from aquadrone_math_utils.angle_math import normalize_angle
-from aquadrone_math_utils.orientation_math import RPY_Matrix
+from scipy.spatial.transform import Rotation
 
 
 class OrientationPIDController:
@@ -20,30 +18,20 @@ class OrientationPIDController:
         if rate is None:
             self.rate = rospy.Rate(10)
 
-        # the pids will be in the relative intrinsic-rotation coordinate frame
-        self.pids = []
-        for angle in ['roll', 'pitch', 'yaw']:
-            Kp = rospy.get_param('/stability/' + angle + '/Kp')
-            Ki = rospy.get_param('/stability/' + angle + '/Ki')
-            Kd = rospy.get_param('/stability/' + angle + '/Kd')
-
-            pid = PID(Kp, Ki, Kd, sample_time=None)
-            pid.output_limits = (-50, 50)
-            pid.setpoint = 0  # all target angles initialized to 0
-            pid.error_map = OrientationPIDController.normalize_angular_error
-            self.pids.append(pid)
-
-        self.rpy = [0, 0, 0]
+        self.target_rotation = Rotation.identity()
+        self.rotation = Rotation.identity()
+        self.omega = np.array([0, 0, 0])
         rospy.Subscriber("/orientation_target", Vector3, callback=self.goal_cb)
         rospy.Subscriber('/state_estimation', SubState, callback=self.state_cb)
         self.pub = rospy.Publisher('/stability_command', Wrench, queue_size=1)
 
     def goal_cb(self, msg):
-        for pid, target_angle in zip(self.pids, [msg.x, msg.y, msg.z]):
-            pid.setpoint = target_angle
+        self.target_rotation = Rotation.from_euler('ZYX', [msg.z, -msg.y, msg.x])
 
     def state_cb(self, msg):
-        self.rpy = [msg.orientation_RPY.x, msg.orientation_RPY.y, msg.orientation_RPY.z]
+        self.rotation = Rotation.from_quat([msg.orientation_quat.w, msg.orientation_quat.x,
+                                            msg.orientation_quat.y, msg.orientation_quat.z])
+        self.omega = np.array([msg.ang_vel.x, msg.ang_vel.y, msg.ang_vel.z])
 
     def run(self):
         control = Wrench()
@@ -59,10 +47,8 @@ class OrientationPIDController:
                 break
 
     def calculate_torque(self):
-        # TODO: use full quaternion based PID, ensure that it works in all cases reliably
-        # quat_error = (self.target_rotation * self.rotation.inv()).as_quat()
-        # axis_error = quat_error[1:] * (1 if quat_error[0] > 0 else -1)
-        # absolute_torque = np.array([pid(orientation) for pid, orientation in zip(self.pids, axis_error)])
-        absolute_torque = np.array([pid(angle) for pid, angle in zip(self.pids, self.rpy)])
-        relative_torque = np.dot(RPY_Matrix(*self.rpy), absolute_torque)
+        quat_error = (self.target_rotation * self.rotation.inv()).as_quat()
+        axis_error = quat_error[1:] * (1 if quat_error[0] > 0 else -1)
+        # Assumes omega is in absolute frame but should be in relative frame
+        relative_torque = -10 * axis_error - 5 * self.rotation.as_matrix() * self.omega
         return relative_torque
