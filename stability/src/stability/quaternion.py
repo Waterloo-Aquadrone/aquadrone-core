178ea35aad8525735eff7acb6faa707f3819e5dc
Amaar Quadri <amaarquadri@gmail.com>
2021-02-04 19:54:38 -0500

    implement more quaternion functionality

--- stability/src/stability/quaternion.py
+++ stability/src/stability/quaternion.py
@@ -1,4 +1,6 @@
 import numpy as np
+from scipy.spatial.transform import Rotation
+from matplotlib import pyplot as plt
 
 
 class Quaternion:
@@ -11,6 +13,10 @@ class Quaternion:
     @staticmethod
     def from_real_imag(q_0, q_vec):
         return Quaternion(q_0, q_vec[0], q_vec[1], q_vec[2])
+
+    @staticmethod
+    def from_array(arr):
+        return Quaternion(arr[0], arr[1], arr[2], arr[3])
 
     def real(self):
         return self.q_0
@@ -37,6 +43,21 @@ class Quaternion:
                          [-self.q_2, -self.q_3,  self.q_0,  self.q_1],
                          [-self.q_3,  self.q_2, -self.q_1,  self.q_0]])
 
+    def as_array(self):
+        return np.array([self.q_0, self.q_1, self.q_2, self. q_3])
+
+    def as_matrix(self):
+        return Rotation.from_quat(self.as_array()).as_matrix()
+
+    def normalize(self):
+        arr = self.as_array()
+        return Quaternion.from_array(arr / np.linalg.norm(arr))
+
+    def as_rpy(self):
+        rpy = Rotation.from_quat(self.as_array()).as_euler('ZYX')[::-1]
+        rpy[1] *= -1
+        return rpy
+
     def __mul__(self, other):
         if type(other) is Quaternion:
             real = self.real() * other.real() - self.imag() * other.imag()
@@ -45,6 +66,18 @@ class Quaternion:
         if type(other) is int or type(other) is float:
             return Quaternion(other * self.q_0, other * self.q_1, other * self.q_2, other * self.q_3)
         raise NotImplemented
+
+    def __add__(self, other):
+        if type(other) is Quaternion:
+            return Quaternion(self.q_0 + other.q_0, self.q_1 + other.q_1,
+                              self.q_2 + other.q_2, self.q_3 + other.q_3)
+        raise NotImplemented
+
+    def __str__(self):
+        return f'[{self.q_0}, {self.q_1}, {self.q_2}, {self.q_3}]'
+
+    def __repr__(self):
+        return f'Quaternion({self.q_0}, {self.q_1}, {self.q_2}, {self.q_3})'
 
     @staticmethod
     def get_omega(q, q_dot):
