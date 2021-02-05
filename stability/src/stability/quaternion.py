7c3f7a4a1ac38afca997631841eceefeba2de7d5
Amaar Quadri <amaarquadri@gmail.com>
2021-02-04 19:54:57 -0500

    add test

--- stability/src/stability/quaternion.py
+++ stability/src/stability/quaternion.py
@@ -99,9 +99,53 @@ class Quaternion:
 
 
 def main():
-    orientation = Quaternion()
-    velocity = Quaternion(q_3=0)
+    data = []
 
+    target_quat = Quaternion.from_array(Rotation.from_euler('ZYX', np.array([0, 0, 0])).as_quat())
+    dt = 0.001
+    # inertia matrix based on SolidWorks model
+    J = np.array([[2850, 1, -25],
+                  [1, 3800, 200],
+                  [-26, 200, 2700]])
+    J_inv = np.linalg.inv(J)
+
+    # Create starting quaternion and angular velocity randomly
+    q = Quaternion.from_array([0.1, 0.1, 0.1, 1]).normalize()
+    omega_vec_prime = np.zeros(3)  # np.random.random(3)
+
+    # PID coefficients
+    k_p = np.array([75, 75, 40])
+    k_d = np.array([150, 1000, 2000])
+
+    for _ in range(int(90 / dt)):
+        # calculate torque based on roll, pitch, yaw
+        # tau_vec_prime = np.dot(q.as_matrix(), -k_p * (q.as_rpy() - target_quat.as_rpy()) - k_d * omega_vec_prime)
+
+        # calculate torque directly from quaternions
+        quat_error = target_quat * q.conj()
+        axis_error = quat_error.imag() * (1 if (quat_error.real() > 0) else -1)
+        tau_vec_prime = -k_p * axis_error - k_d * omega_vec_prime
+
+        q_dot = Quaternion.from_array((1/2) * np.dot(q.G().transpose(), omega_vec_prime))
+        omega_vec_prime_dot = np.dot(J_inv, tau_vec_prime) - \
+            np.dot(J_inv, np.cross(omega_vec_prime, np.dot(J, omega_vec_prime)))
+        q += q_dot * dt
+        q = q.normalize()
+        omega_vec_prime += omega_vec_prime_dot * dt
+
+        data.append(q.as_array())
+        if np.linalg.norm(q_dot.as_array()) > 100_000:
+            print('Warning: unstable!')
+            break
+
+    for i in range(4):
+        plt.plot(dt * np.arange(len(data)), [elem[i] for elem in data], label=f'$q_{i}$')
+    plt.xlabel('Time (s)')
+    plt.legend()
+    plt.show()
+
+    print('Target Quaternion:', target_quat)
+    print('Final Quaternion Achieved:', q)
 
 
 if __name__ == '__main__':
