f30a424b0da55bef55b11e53a0cece24f4a31372
Amaar Quadri <amaarquadri@gmail.com>
2021-02-09 01:07:27 -0500

    negate torque, use absolute torque

--- stability/src/stability/orientation_pid_controller.py
+++ stability/src/stability/orientation_pid_controller.py
@@ -54,6 +54,7 @@ class OrientationPIDController:
         x, y, z, w = quat_error
         axis_error = np.array([x, y, z]) * (1 if w > 0 else -1)
         # Assumes omega is in absolute frame but should be in relative frame
-        relative_torque = -self.k_ps * axis_error - self.k_ds * np.dot(self.rotation.as_matrix(), self.omega)
-        print(f'Target: {self.target_rotation.as_quat()}, current: {self.rotation.as_quat()}, torque: {relative_torque}')
-        return relative_torque
+        relative_torque = self.k_ps * axis_error + self.k_ds * np.dot(self.rotation.as_matrix(), self.omega)
+        absolute_torque = self.rotation.inv().apply(relative_torque)
+        print(f'Target: {self.target_rotation.as_quat()}, current: {self.rotation.as_quat()}, torque: {absolute_torque}')
+        return absolute_torque
