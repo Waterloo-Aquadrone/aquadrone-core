34e7d847cfd6c1c6af2795150b7e1936a480873e
Amaar Quadri <amaarquadri@gmail.com>
2021-02-14 17:42:52 -0500

    rename faker to omniscient_vision.py, separate code from node

---
+++ vision/nodes/omniscient_vision_node.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import rospy
+from vision.omniscient_vision import OmniscientVision
+
+
+if __name__ == "__main__":
+    rospy.init_node("omniscient_vision_node")
+
+    faker = OmniscientVision()
+    faker.run()
