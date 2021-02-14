0d1e4ffa0ac49a6424d0af741d2b1f3acbfe5288
Amaar Quadri <amaarquadri@gmail.com>
2021-02-14 18:06:58 -0500

    update variable name

--- vision/nodes/omniscient_vision_node.py
+++ vision/nodes/omniscient_vision_node.py
@@ -6,5 +6,5 @@ from vision.omniscient_vision import OmniscientVision
 if __name__ == "__main__":
     rospy.init_node("omniscient_vision_node")
 
-    faker = OmniscientVision()
-    faker.run()
+    omniscient_vision_node = OmniscientVision()
+    omniscient_vision_node.run()
