85f1535c38c1091f923c3a70fe35e299bb90f9ba
Amaar Quadri <amaarquadri@gmail.com>
2021-02-28 23:41:36 -0500

    added plotting to barrel_roll_test.py

--- path_planning/nodes/barrel_roll_test.py
+++ path_planning/nodes/barrel_roll_test.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 import rospy
+import rospkg
+from time import time
+import matplotlib.pyplot as plt
 
 from path_planning.states.waiting_state import WaitingState
 from path_planning.states.stabilize_state import StabilizeState
@@ -15,13 +18,23 @@ from path_planning.state_machines.branching_state_machine import BranchingStateM
 from path_planning.state_executor import StateExecutor
 
 
+def plot_depth_data(data):
+    directory = rospkg.RosPack().get_path('path_planning')
+    for angle in ['roll', 'pitch', 'yaw']:
+        plt.plot(data['t'], data[angle], label=angle)
+    plt.xlabel('Time (s)')
+    plt.ylabel('Angle (rad)')
+    plt.legend()
+    plt.title('Orientation Versus Time During Barrel Roll')
+    plt.savefig(directory + '/barrel-roll-' + str(time()) + '.png')
+
+
 if __name__ == "__main__":
     rospy.init_node("barrel_roll_test")
 
     timeout = 120  # seconds
 
     stabilise_at_depth_machine = ParallelStateMachine('stabilise_at_depth', [StabilizeState(), GoToDepthState(-3)])
-    dive_machine = SequentialStateMachine('dive', [WaitingState(20), stabilise_at_depth_machine])
     timed_barrel_roll_state = TimedStateMachine(BarrelRoll(), timeout, timeout_exit_code=1)
     dive_and_roll_machine = SequentialStateMachine('dive_and_roll', [WaitingState(20),
                                                                      stabilise_at_depth_machine,
@@ -29,10 +42,12 @@ if __name__ == "__main__":
     success_surface_machine = SequentialStateMachine('surface', [GoToDepthState(0), WaitingState(10), ExitCodeState(0)])
     failure_surface_machine = SequentialStateMachine('stabilize_and_surface', [StabilizeState(), GoToDepthState(0),
                                                                                WaitingState(10), ExitCodeState(1)])
-
     machine = BranchingStateMachine('barrel_roll_test', dive_and_roll_machine, success_surface_machine,
                                     failure_surface_machine)
-    logging_machine = machine = ParallelStateMachine('logging_state_machine', [machine], daemon_states=[DataLogger()])
+
+    data_logger = DataLogger()
+    data_logger.add_data_post_processing_func(plot_depth_data)
+    logging_machine = ParallelStateMachine('logging_state_machine', [machine], daemon_states=[data_logger])
 
     # TODO: add flowchart generation once BranchingStateMachine is supported
     executor = StateExecutor(logging_machine, rospy.Rate(5))
