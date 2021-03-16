14b2d7ad9abb085af49360c82e9691dd42fb356d
alvin ubuntu <alvin.ipad2@gmail.com>
2021-03-15 21:22:08 -0400

    update test cases

--- aquadrone_math_utils/test/test_orientation_math.py
+++ aquadrone_math_utils/test/test_orientation_math.py
@@ -17,16 +17,23 @@ class TestOMath(unittest.TestCase):
 
     def check_omath_rotation(self, quat_vec):
         rotation_quat_to_euler = Rotation.from_quat(quat_vec[1:] + [quat_vec[0]]).as_euler('ZYX')
-        quat_vec = np.array(quat_vec)
-        omath_quat_to_euler = np.array(OMath.quaternion_to_euler(quat_vec/np.sum(quat_vec*quat_vec)**0.5), dtype=float)
+        omath_quat_to_euler = np.array(OMath.quaternion_to_euler(np.array(quat_vec)), dtype=float)
         omath_equal_rotation = np.all(np.isclose(omath_quat_to_euler,rotation_quat_to_euler))
         self.assertTrue(omath_equal_rotation)
 
+    def to_unit_vec(self, quat_vec):
+        quat_vec = np.array(quat_vec)
+        unit_vec = quat_vec / np.sum(quat_vec*quat_vec)**0.5
+        return unit_vec.tolist()
+
     def test_quat_to_euler(self):
-        self.check_omath_rotation([0.1, 0.2, 0.3, 0.4])
-        self.check_omath_rotation([0.1, -0.2, 0.15, 0.1])
-        self.check_omath_rotation([0.5, 0.5, 0.5, -0.5])
-        self.check_omath_rotation([0.5, -0.5, 0.5, 0.5])
+        self.check_omath_rotation(self.to_unit_vec([0.1, 0.2, 0.3, 0.4]))
+        self.check_omath_rotation(self.to_unit_vec([0.1, -0.2, 0.15, 0.1]))
+        self.check_omath_rotation(self.to_unit_vec([0.5, 0.5, 0.5, -0.5]))
+        # sinp = -1 case
+        self.check_omath_rotation(self.to_unit_vec([-0.1, 0.5**0.5, 0, 0.7]))
+        # sinp = 1 case
+        self.check_omath_rotation(self.to_unit_vec([0, 0.5/0.7, 0, 0.7]))
 
 
 if __name__ == '__main__':
