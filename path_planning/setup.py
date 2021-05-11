d402676acb8ea6922117129efa885296fdf3ec8f
jdumanski <Willowdale12!>
2021-05-11 18:39:39 -0400

    comment change 2

--- path_planning/setup.py
+++ path_planning/setup.py
@@ -1,6 +1,7 @@
 from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 #comment
+#next comment
 d = generate_distutils_setup()
 d['packages'] = ['path_planning']
 d['package_dir'] = {'': 'src'}
