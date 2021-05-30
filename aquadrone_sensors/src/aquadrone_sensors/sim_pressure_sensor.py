61dea9d331b082d1a196b290bc4efcff1c18e324
alvin ubuntu <alvin.ipad2@gmail.com>
2021-05-30 18:43:30 -0400

    added rotated offset to sim pressure sensor

--- aquadrone_sensors/src/aquadrone_sensors/sim_pressure_sensor.py
+++ aquadrone_sensors/src/aquadrone_sensors/sim_pressure_sensor.py
@@ -1,7 +1,7 @@
 import rospy
 from gazebo_msgs.msg import ModelStates
 from sensor_msgs.msg import FluidPressure
-
+from aquadrone_math_utils.quaternion import Quaternion
 
 class SimPressureSensor:
     WATER_SPECIFIC_GRAVITY = 1000 * 9.81  # Pa/m
@@ -14,11 +14,16 @@ class SimPressureSensor:
         self.object_pos_sub = rospy.Subscriber("gazebo/model_states", ModelStates, self.get_obj_pos)
         self.pub = rospy.Publisher("/aquadrone/sensors/pressure", FluidPressure, queue_size=1)
         self.measurement_z = 0
+        pressure_offset_dict = rospy.get_param("/submarine/pressure_sensor_offset")
+        self.pressure_offset = [pressure_offset_dict["x"], pressure_offset_dict["y"], pressure_offset_dict["z"]]
 
     def get_obj_pos(self, data):
         for name, pose in zip(data.name, data.pose):
             if name == self.sub_name:
-                self.measurement_z = pose.position.z
+                quat = [pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w]
+                quad_orientation = Quaternion.from_array(quat)
+                rotated_offset = quad_orientation.rotate(self.pressure_offset)
+                self.measurement_z = pose.position.z + rotated_offset[2]
 
     def publish_message(self):
         depth = -self.measurement_z
