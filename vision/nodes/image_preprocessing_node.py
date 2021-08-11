85a166a20d4593d8487c65814feb3905275e36fb
msmel01 <ms.elly17.01@gmail.com>
2021-08-11 10:24:58 -0400

    debugged

--- vision/nodes/image_preprocessing_node.py
+++ vision/nodes/image_preprocessing_node.py
@@ -1,7 +1,8 @@
+#!/usr/bin/env python3
 import rospy
-import sensor_msgs.Image as Image
-from cv_bridge import CvBridge, CvBridgeError
 import cv2
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
 # TODO:
 # calibrate blur_threshold
@@ -10,26 +11,27 @@ import cv2
 # Smoothing such as blur
 
 class ImagePreprocessingNode():
-    def __init(self):
-        self.name = 'Image Preprocessing'
+    def __init__(self):
+        self.name = 'Image Preprocessing Node'
         self.blur_threshold = 100
         rospy.init_node(self.name)
         self.bridge = CvBridge()
         self.pub = rospy.Publisher('vision/preprocess', Image, queue_size=1)
+        rospy.Subscriber('vision/test', Image, self.process_image, queue_size=1) # for test
         rospy.Subscriber('/zed/zed_node/rgb/image_rect_color', Image, self.process_image, queue_size=1)
 
 
     def process_image(self, image):
         try:
-            frame = self.bridge.imgmsg_to_cv2(image, desired_encoding='passthrough')
+            frame = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
             frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             focus_measure = cv2.Laplacian(frame_gray, cv2.CV_64F).var()
             if focus_measure > self.blur_threshold:
                 # put preprocessing here
-                image_preprocess = self.bridge.cv2_to_imgmsg(frame, encoding='passthrough')
+                image_preprocess = self.bridge.cv2_to_imgmsg(frame, encoding='bgr8')
                 self.pub.publish(image_preprocess)
             else:
-                pass # how to skip this frame and wait for the other?
+                pass
 
         except CvBridgeError as e:
             print(e)
