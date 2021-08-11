c56e1f60cb242ce336797b751cf04834ccdad05d
msmel01 <ms.elly17.01@gmail.com>
2021-08-11 10:24:12 -0400

    test image preprocessing node

--- vision/nodes/test_output_node.py
+++ vision/nodes/test_output_node.py
@@ -4,13 +4,20 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+import aquadrone_msgs.BoundingBox as BoundingBox
+import aquadrone_msgs.BoundingBoxes as BoundingBoxes
 
 class TestOutputNode():
     def __init__(self):
         self.name = 'Test Output Node'
         rospy.init_node(self.name, log_level=rospy.DEBUG)
         self.bridge = CvBridge()
-        rospy.Subscriber('vision/test', Image, self.show_image) # to test publish_test_data_node
+
+        # to test publish_test_data_node
+        # rospy.Subscriber('vision/test', Image, self.show_image)
+
+        # to test image_preprocessing_node
+        rospy.Subscriber('vision/preprocess', Image, self.show_image)
 
 
     def show_image(self, image):
