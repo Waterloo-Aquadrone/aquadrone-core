a535c402a06f2fce8882626c04ed622417ddc936
msmel01 <ms.elly17.01@gmail.com>
2021-08-12 10:32:37 -0400

    added test for object detection node

--- vision/nodes/test_output_node.py
+++ vision/nodes/test_output_node.py
@@ -4,20 +4,25 @@ import cv2
 import numpy as np
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
-import aquadrone_msgs.BoundingBox as BoundingBox
-import aquadrone_msgs.BoundingBoxes as BoundingBoxes
+from aquadrone_msgs.msg import BoundingBox, BoundingBoxes
 
 class TestOutputNode():
     def __init__(self):
-        self.name = 'Test Output Node'
+        self.name = 'test_output_node'
+        self.classes_file_path = './testdata/coco.names'
         rospy.init_node(self.name, log_level=rospy.DEBUG)
         self.bridge = CvBridge()
+        with open(self.classes_file_path, 'r') as classes_file:
+            self.classes = [cls.strip() for cls in classes_file.readlines()]
 
-        # to test publish_test_data_node
+        # uncomment to test publish_test_data_node (comment other topics)
         # rospy.Subscriber('vision/test', Image, self.show_image)
 
-        # to test image_preprocessing_node
-        rospy.Subscriber('vision/preprocess', Image, self.show_image)
+        # uncomment o test image_preprocessing_node (comment other topics)
+        # rospy.Subscriber('vision/preprocess', Image, self.show_image)
+
+        # uncomment to test object detection code (comment other topics)
+        rospy.Subscriber('vision/bounding_boxes', BoundingBoxes, self.show_bboxes)
 
 
     def show_image(self, image):
@@ -25,6 +30,26 @@ class TestOutputNode():
         img = self.bridge.imgmsg_to_cv2(image, desired_encoding='bgr8')
         cv2.imshow('image', img)
         cv2.waitKey(5000)
+        cv2.destroyAllWindows()
+
+
+    def show_bboxes(self, bboxes):
+        frame = self.bridge.imgmsg_to_cv2(bboxes.frame, desired_encoding='bgr8')
+
+        for bbox in bboxes.boxes:
+            id = bbox.class_id
+            x = bbox.x_center
+            y = bbox.y_center
+            w = bbox.width
+            h = bbox.height
+            color = list(np.random.random(size=3) * 256)
+            cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
+            text = "{}".format(self.classes[id])
+            cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 1)
+
+        cv2.imshow('image', frame)
+        cv2.waitKey(5000)
+        cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
